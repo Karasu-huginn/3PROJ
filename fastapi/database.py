249535d134bb44736232c9ee835b8268ef65af32
erwann quin <erwann.quin@supinfo.com>
2026-03-17 14:45:18 +0100

    refactor: moved db infos to dotenv file

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = 'postgresql://postgres:admin@localhost:5432/SCRUMDatabase'
+def get_db_url():
+    load_dotenv(".env.local.db")
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    port = os.getenv("PORT")
+    db_name = os.getenv("DB_NAME")
+    host = os.getenv("HOST")
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+URL_DATABASE = get_db_url()
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)

e98669cda3acf9be24f8ebc79c7f46a1d9e93bb1
Savaira <savaira.talaat@gmail.com>
2026-06-07 12:11:04 +0200

    add: get_optional_user use for not authenticated user

--- fastapi/auth/dependencies.py
+++ fastapi/auth/dependencies.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from jose import JWTError
-from typing import Annotated
+from typing import Annotated, Optional
 
 import models
 from database import SessionLocal
@@ -46,5 +46,35 @@ def get_current_user(
     user = db.query(models.Users).filter(models.Users.id == int(user_id)).first()
     if user is None or not user.is_active:
         raise credentials_exception
+
+    return user
+
+def get_optional_user(
+    db: Annotated[Session, Depends(get_db)],
+    credentials: Annotated[
+        Optional[HTTPAuthorizationCredentials],
+        Depends(HTTPBearer(auto_error=False)),  # auto_error=False → None si pas de header
+    ] = None,
+) -> Optional[models.Users]:
+    if credentials is None:
+        return None
+
+    try:
+        payload = decode_access_token(credentials.credentials)
+        user_id: str = payload.get("sub")
+        jti: str = payload.get("jti")
+
+        if user_id is None or jti is None:
+            return None
+
+    except JWTError:
+        return None
+
+    if auth_service.is_token_revoked(db, jti):
+        return None
+
+    user = db.query(models.Users).filter(models.Users.id == int(user_id)).first()
+    if user is None or not user.is_active:
+        return None
 
     return user
